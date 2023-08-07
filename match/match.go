467bb860f4f3ddfe1177172188a0057fa531ba49
Armin Becher <Armin Becher>
2023-08-07 20:03:30 +0200

    feat: add matcher

---
+++ match/match.go
@@ -0,0 +1,81 @@
+package match
+
+// Match is a simple expression which evaluates to be either 'true' or 'false'
+type Match[T any] func(e T) bool
+
+// And combines two expressions with a logical AND `&&`
+func (m Match[T]) And(o Match[T]) Match[T] {
+	if o == nil {
+		return m
+	}
+	return func(e T) bool { return m(e) && o(e) }
+}
+
+// Or combines two expressions with a logical OR `||`
+func (m Match[T]) Or(o Match[T]) Match[T] {
+	if o == nil {
+		return m
+	}
+	return func(e T) bool { return m(e) || o(e) }
+}
+
+// Not negates a logical expression
+func Not[T any](o Match[T]) Match[T] {
+	if o == nil {
+		return func(a T) bool { return false }
+	}
+	return func(e T) bool { return !o(e) }
+}
+
+// Eq returns a function which will be true if `other` is equal to the input
+func Eq[T comparable](other T) Match[T] {
+	return func(val T) bool {
+		return val == other
+	}
+}
+
+// EAnyOfq returns a function which will be true if any value of `other` is equal to the input
+func AnyOf[T comparable](other ...T) Match[T] {
+	return func(val T) bool {
+		for _, o := range other {
+			if val == o {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+// Slice
+
+// Eq returns a function which will be true if all elements of `other` are equal to the input
+func SliceEq[T ~[]E, E comparable](other T) Match[T] {
+	return func(val T) bool {
+		if len(val) != len(other) {
+			return false
+		}
+		for i := range val {
+			if val[i] != other[i] {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// Maps
+
+// MapEq returns a function which will be true if `other` contains the same key/value pairs as in the input
+func MapEq[T ~map[K]V, K, V comparable](other T) Match[T] {
+	return func(val T) bool {
+		if len(val) != len(other) {
+			return false
+		}
+		for k, v1 := range val {
+			if v2, ok := other[k]; !ok || v1 != v2 {
+				return false
+			}
+		}
+		return true
+	}
+}
---
+++ match/match_test.go
@@ -0,0 +1,60 @@
+package match_test
+
+import (
+	"testing"
+
+	"github.com/becheran/smock/match"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEq(t *testing.T) {
+	c := make(chan bool)
+
+	assert.True(t, match.Eq("foo")("foo"))
+	assert.True(t, match.Eq(1)(1))
+	assert.True(t, match.Eq("foo")("foo"))
+	assert.True(t, match.Eq("foo")("foo"))
+	assert.True(t, match.Eq(c)(c))
+	assert.True(t, match.Eq([2]string{"foo", "bar"})([2]string{"foo", "bar"}))
+
+	assert.False(t, match.Eq(c)(nil))
+	assert.False(t, match.Eq(c)(make(chan bool)))
+	assert.False(t, match.Eq([2]string{"foo", "bar"})([2]string{"foo", "bz"}))
+	assert.False(t, match.Eq[string]("")("foo"))
+	assert.False(t, match.Eq[string]("foo")("bar"))
+	assert.False(t, match.Eq[any]("foo")(1))
+}
+
+func TestAnyOf(t *testing.T) {
+	assert.True(t, match.AnyOf("foo", "bar")("bar"))
+	assert.False(t, match.AnyOf("foo", "bar")("baz"))
+}
+
+func TestOr(t *testing.T) {
+	assert.True(t, match.Eq("foo").Or(match.Eq("bar"))("foo"))
+	assert.True(t, match.Eq("foo").Or(match.Eq("bar"))("bar"))
+	assert.False(t, match.Eq("foo").Or(match.Eq("bar"))("other"))
+}
+func TestNot(t *testing.T) {
+	assert.False(t, match.Not(match.Eq("foo"))("foo"))
+	assert.True(t, match.Not(match.Eq("foo"))("bar"))
+}
+
+func TestAnd(t *testing.T) {
+	assert.True(t, match.AnyOf("foo", "bar").And(match.AnyOf("bar", "foo"))("foo"))
+	assert.False(t, match.AnyOf("foo", "bar").And(match.AnyOf("bar", "f"))("foo"))
+}
+
+func TestSliceEq(t *testing.T) {
+	assert.True(t, match.SliceEq[[]int](nil)(nil))
+	assert.False(t, match.SliceEq([]int{1, 2})(nil))
+	assert.False(t, match.SliceEq([]int{1, 2})([]int{1, 2, 3}))
+}
+
+func TestMapEq(t *testing.T) {
+	assert.True(t, match.MapEq[map[string]int](nil)(nil))
+	assert.False(t, match.MapEq(map[int]int{0: 1})(nil))
+	assert.False(t, match.MapEq(map[int]int{0: 1})(map[int]int{1: 1}))
+	assert.False(t, match.MapEq(map[int]int{0: 1})(map[int]int{1: 1, 0: 1}))
+	assert.True(t, match.MapEq(map[int]int{0: 1})(map[int]int{0: 1}))
+}
