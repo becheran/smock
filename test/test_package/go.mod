ca1665a4d53060f0ec7516d1c4d339614f8eb003
Armin Becher <Armin Becher>
2023-09-27 21:45:45 +0200

    test: test with go 1.18 and use smock from project

--- test/test_package/go.mod
+++ test/test_package/go.mod
@@ -1,11 +1,16 @@
 module github.com/test/testpackage
 
-go 1.20
+go 1.18
 
-require github.com/stretchr/testify v1.8.4
+require (
+	github.com/becheran/smock v0.0.0-20230927163458-39f3c48af310
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/becheran/smock => ../../
--- test/test_package/testpackage_test.go
+++ test/test_package/testpackage_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/becheran/smock/match"
 	"github.com/stretchr/testify/assert"
-	"github.com/test/testpackage/cmp"
 	testpackage_mock "github.com/test/testpackage/golden_test"
 )
 
@@ -13,15 +13,15 @@ func TestSimpleWhen(t *testing.T) {
 	m := testpackage_mock.NewMockSimple(t)
 	m.WHEN().
 		Baz().
-		Expect(cmp.Eq(1), nil).
+		Expect(match.Eq(1), nil).
 		Return("1")
 	m.WHEN().
 		Baz().
-		Expect(cmp.Eq(2), nil).
+		Expect(match.Eq(2), nil).
 		Return("2")
 	m.WHEN().
 		Baz().
-		Expect(cmp.Eq(23).Or(cmp.Eq(3)).Or(cmp.Eq(2)), nil).
+		Expect(match.Eq(23).Or(match.Eq(3)).Or(match.Eq(2)), nil).
 		Return("2")
 	assert.Equal(t, "1", m.Baz(1, "foo"))
 	assert.Equal(t, "2", m.Baz(2, "bzs"))
@@ -32,7 +32,7 @@ func TestFallBackToMatchAll(t *testing.T) {
 	m := testpackage_mock.NewMockSimple(t)
 	m.WHEN().
 		Baz().
-		Expect(cmp.Eq(1), nil).
+		Expect(match.Eq(1), nil).
 		Return("1")
 	m.WHEN().
 		Baz().
@@ -69,9 +69,9 @@ func TestFallMatchAllTwiceError(t *testing.T) {
 func TestLambda(t *testing.T) {
 	m := testpackage_mock.NewMockWithLambda[string](t)
 
-	m.WHEN().Foo().Expect(cmp.Eq(1), cmp.Eq("a")).Return(true)
-	m.WHEN().Bar().Expect(cmp.Eq(struct{}{}), cmp.Eq(struct{}{})).Return(true)
-	m.WHEN().Baz().Expect(cmp.Eq("other")).Return(true)
+	m.WHEN().Foo().Expect(match.Eq(1), match.Eq("a")).Return(true)
+	m.WHEN().Bar().Expect(match.Eq(struct{}{}), match.Eq(struct{}{})).Return(true)
+	m.WHEN().Baz().Expect(match.Eq("other")).Return(true)
 
 	assert.True(t, m.Foo(1, "a"))
 	assert.True(t, m.Bar(struct{}{}, struct{}{}))
